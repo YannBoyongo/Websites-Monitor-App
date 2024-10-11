38e02a2e8562a4cc4512c47372dbbf60d2090fb1
Yann Boyongo <yaboyon3@gmail.com>
2024-10-11 10:37:26 +0200

    adding the plainadmin them to the app

---
+++ database/migrations/2024_10_11_082638_create_websites_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->boolean('is_up')->default(true);
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('websites');
+    }
+};
